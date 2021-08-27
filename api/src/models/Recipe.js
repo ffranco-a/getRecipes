3ce2ea650c603cac3a07cc34b307b6644caa278c
ffranco-a <84511984+ffranco-a@users.noreply.github.com>
2021-08-27 16:56:06 -0300

    database almost ready

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,25 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
+module.exports = sequelize => {
   // defino el modelo
   sequelize.define('recipe', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    summary: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    spoonacularScore: {
+      type: DataTypes.INTEGER,
+    },
+    healthScore: {
+      type: DataTypes.INTEGER,
+    },
+    pasoAPaso: {
+      type: DataTypes.TEXT,
     },
   });
 };
